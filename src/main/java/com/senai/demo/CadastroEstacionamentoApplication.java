a373c85a0897a30b2c7826f3e599ed89ed7eab17
PHCN <pedrohenrique.cn07@gmail.com>
2025-08-05 15:06:49 -0300

    Dia 05/08/2025 criação de uma API para a aula

---
+++ src/main/java/com/senai/demo/CadastroEstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.senai.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroEstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroEstacionamentoApplication.class, args);
+	}
+
+}
